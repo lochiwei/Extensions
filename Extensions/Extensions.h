3cf8337969372b6d60acaa32f1e53206c73758ea
lochiwei <lochiwei.ios.dev.free@gmail.com>
2018-07-28 20:45:10 +0800

    Initial Commit

---
+++ Extensions/Extensions.h
@@ -0,0 +1,19 @@
+//
+//  Extensions.h
+//  Extensions
+//
+//  Created by pegasus on 2018/07/28.
+//  Copyright © 2018年 Lo Chiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Extensions.
+FOUNDATION_EXPORT double ExtensionsVersionNumber;
+
+//! Project version string for Extensions.
+FOUNDATION_EXPORT const unsigned char ExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Extensions/PublicHeader.h>
+
+
