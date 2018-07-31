1a42908fc2f4931722d18bc0193ca859e699fd48
lochiwei <lochiwei.ios.dev.free@gmail.com>
2018-07-31 16:08:48 +0800

    add primitive nodes to SCNNode+Toys add newLightNode(), newCameraNode() to SCNNode+Extensions

--- Extensions/Playgrounds/SCNNode+Toys.swift
+++ Extensions/Playgrounds/SCNNode+Toys.swift
@@ -17,4 +17,97 @@ extension SCNNode {
         return node
     }
     
+    // MARK: - Primitive Nodes
+    
+    // floor
+    public func newFloorNode(at position:SCNVector3? = nil, diffuse contents:Any? = nil) -> SCNNode {
+        
+        // geometry
+        let geo = SCNFloor()
+        
+        // node
+        let node = newShape(geo, at: position, diffuse: contents)
+        node.name = "floor"
+        
+        return node
+    }
+    
+    // box
+    public func newBoxNode(_ width:CGFloat, _ height:CGFloat, _ length:CGFloat, at position:SCNVector3? = nil, diffuse contents:Any? = nil) -> SCNNode {
+        
+        // geometry
+        let geo = SCNBox(width: width, height: height, length: length, chamferRadius: 0)
+        
+        // node
+        let node = newShape(geo, at: position, diffuse: contents)
+        node.name = "box"
+        
+        return node
+    }
+    
+    // pyramid
+    public func newPyramidNode(_ width:CGFloat, _ height:CGFloat, _ length:CGFloat, at position:SCNVector3? = nil, diffuse contents:Any? = nil) -> SCNNode {
+        
+        // geometry
+        let geo = SCNPyramid(width: width, height: height, length: length)
+        
+        // node
+        let node = newShape(geo, at: position, diffuse: contents)
+        node.name = "pyramid"
+        
+        return node
+    }
+    
+    // sphere
+    public func newSphereNode(_ radius:CGFloat, at position:SCNVector3? = nil, diffuse contents:Any? = nil) -> SCNNode {
+        
+        // geometry
+        let geo = SCNSphere(radius: radius)
+        
+        // node
+        let node = newShape(geo, at: position, diffuse: contents)
+        node.name = "sphere"
+        
+        return node
+    }
+    
+    // cylinder
+    public func newCylinderNode(r:CGFloat, h:CGFloat, at position:SCNVector3? = nil, diffuse contents:Any? = nil) -> SCNNode {
+        
+        // geometry
+        let geo = SCNCylinder(radius: r, height: h)
+        
+        // node
+        let node = newShape(geo, at: position, diffuse: contents)
+        node.name = "cylinder"
+        
+        return node
+    }
+    
+    // tube
+    public func newTubeNode(r:CGFloat, R:CGFloat, h:CGFloat, at position:SCNVector3? = nil, diffuse contents:Any? = nil) -> SCNNode {
+        
+        // geometry
+        let geo = SCNTube(innerRadius: r, outerRadius: R, height: h)
+        
+        // node
+        let node = newShape(geo, at: position, diffuse: contents)
+        node.name = "tube"
+        
+        return node
+    }
+    
+    // torus
+    public func newTorusNode(R:CGFloat, r:CGFloat, at position:SCNVector3? = nil, diffuse contents:Any? = nil) -> SCNNode {
+        
+        // geometry
+        let geo = SCNTorus(ringRadius: R, pipeRadius: r)
+        
+        // node
+        let node = newShape(geo, at: position, diffuse: contents)
+        node.name = "torus"
+        
+        return node
+    }
+    
 }// end: extension SCNNode
--- Extensions/SceneKit/SCNNode+Extensions.swift
+++ Extensions/SceneKit/SCNNode+Extensions.swift
@@ -8,15 +8,8 @@ extension SCNNode {
     // usage: node.add(particleSystem)
     public func add(_ particalSystem: SCNParticleSystem) { addParticleSystem(particalSystem) }
     
-    // Animations
-    // ----------
-    
-    // usage: node.run(action)
-    public func run(_ action:SCNAction) { runAction(action) }
-    
-    
-    // Geometry & Coordinate System
-    // ----------------------------
+    // MARK: - Camera
+    // --------------
     
     // note: there is another (official) look(at: SCNVector3) method.
     //       see what's the difference
@@ -35,6 +28,54 @@ extension SCNNode {
         lock.isGimbalLockEnabled = true
         
         constraints = [lock]
+    }// end: look(at:)
+    
+    // add camera to node
+    // usage: let cameraNode = node.newCameraNode(at: position)
+    public func newCameraNode(at position: SCNVector3? = nil, lookAt target: SCNNode? = nil) -> SCNNode {
+        
+        // camera
+        let camera = SCNCamera()
+        
+        // camera node
+        let cameraNode = SCNNode()
+        cameraNode.camera = camera
+        if let p = position { cameraNode.position = p }
+        if let t = target { cameraNode.look(at: t) }
+        
+        // graph tree
+        add(cameraNode)
+        
+        return cameraNode
     }
+    
+    // MARK: - Lights
+    // --------------
+    
+    // usage: let lightNode = node.newLightNode(.omni, at: position)
+    public func newLightNode(_ type: SCNLight.LightType, color: Any? = nil, at position:SCNVector3? = nil) -> SCNNode {
+        
+        // light
+        let light = SCNLight()
+        light.type = type
+        if let c = color { light.color = c }
+        
+        // light node
+        let lightNode = SCNNode()
+        lightNode.light = light
+        if let p = position { lightNode.position = p }
+        
+        // graph tree
+        add(lightNode)
+        
+        return lightNode
+    }
+    
+    // MARK: - Animations
+    // ----------
+    
+    // usage: node.run(action)
+    public func run(_ action:SCNAction) { runAction(action) }
+    
     
 }// end: extension SCNNode
